034b77e14a1c72c2b36e30cb045f370704fe5427
Maryam Majidi <maryam.majidi@uni-kassel.de>
2021-11-01 11:53:53 +0100

    simplify make.jl to check the host

--- docs/make.jl
+++ docs/make.jl
@@ -3,35 +3,35 @@ using Documenter, PandaModels
 makedocs(
     modules = [PandaModels],
     authors = "MMajidi137",
-    repo = "https://github.com/MMajidi137/PandaModels.jl/blob/{commit}{path}#L{line}",
+    # repo = "https://github.com/MMajidi137/PandaModels.jl/blob/{commit}{path}#L{line}",
     sitename = "PandaModels.jl",
-    format = Documenter.HTML(
-        prettyurls = get(ENV, "CI", nothing) == "true",
-        canonical="https://mmajidi137.github.io/PandaModels.jl/stable/",
-        assets = String[],
-    ),
-    pages = [
-        "Home" => "index.md",
-        "Manual" => ["Getting Started" => "quickguide.md"],
-        "Tutorials" => [
-            "Power Flow" => "pf.md",
-            "Optimal Power Flow" => "opf.md",
-            "Optimal Transmission Switching" => "ots.md",
-            "Timeseries and Multinetwork" => "ts_mn.md",
-            "Optimal MultiNetwork Storage" => "omns.md",
-            "Transmission Network Expansion Planning" => "tnep.md",
-            "Optimal Voltage Deviation" => "vd.md",
-            "Radial Distribution Network" => "rds.md",
-        ],
-        "Developer" => [
-            "Develop Mode" => "develop.md",
-            "Model Guidlines" => "model.md",
-            "Call Model in pandapower" => "modelpp.md",
-            "Add Test" => "test.md",
-        ],
-    ],
-    doctest = true,
-    linkcheck = true,
+    format = Documenter.HTML(),
+    # format = Documenter.HTML(
+    #     prettyurls = get(ENV, "CI", nothing) == "true",
+    #     canonical="https://mmajidi137.github.io/PandaModels.jl/stable/",
+    #     assets = String[],
+    # ),
+    pages = ["Home" => "index.md"]
+        # "Manual" => ["Getting Started" => "quickguide.md"],
+        # "Tutorials" => [
+        #     "Power Flow" => "pf.md",
+        #     "Optimal Power Flow" => "opf.md",
+        #     "Optimal Transmission Switching" => "ots.md",
+        #     "Timeseries and Multinetwork" => "ts_mn.md",
+        #     "Optimal MultiNetwork Storage" => "omns.md",
+        #     "Transmission Network Expansion Planning" => "tnep.md",
+        #     "Optimal Voltage Deviation" => "vd.md",
+        #     "Radial Distribution Network" => "rds.md",
+        # ],
+        # "Developer" => [
+        #     "Develop Mode" => "develop.md",
+        #     "Model Guidlines" => "model.md",
+        #     "Call Model in pandapower" => "modelpp.md",
+        #     "Add Test" => "test.md",
+        # ],
+    # ],
+    # doctest = true,
+    # linkcheck = true,
     # format = Documenter.HTML(
     #     # See https://github.com/JuliaDocs/Documenter.jl/issues/868
     #     prettyurls = get(ENV, "CI", nothing) == "true",
@@ -42,5 +42,5 @@ makedocs(
 
 deploydocs(
     repo = "github.com/MMajidi137/PandaModels.git",
-    push_preview = true
+    # push_preview = true
 )

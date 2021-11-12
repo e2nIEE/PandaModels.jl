eccc57d459beb8335a08a91ee20522fee7a13841
Maryam Majidi <maryam.majidi@uni-kassel.de>
2021-11-12 18:15:25 +0100

    update run test

--- test/runtests.jl
+++ test/runtests.jl
@@ -8,17 +8,16 @@ pdm_path = joinpath(dirname(pathof(PandaModels)), "..")
 data_path = joinpath(pdm_path, "test", "data")
 
 case_pm = joinpath(data_path, "test_pm.json")
-case_pf = joinpath(data_path, "test_pf.json")
-case_opf = joinpath(data_path, "test_opf.json")
-case_custom = joinpath(data_path, "test_custom.json")
-case_ots = joinpath(data_path, "test_ots.json")
-case_tnep = joinpath(data_path, "test_tnep.json")
+case_pf_ac = joinpath(data_path, "test_pf_ac.json")
+case_opf_ac = joinpath(data_path, "test_opf_ac.json")
+case_tnep_ac = joinpath(data_path, "test_tnep_ac.json")
+case_ots_dc = joinpath(data_path, "test_ots_dc.json")
 case_vd = joinpath(data_path, "test_vd.json")
 
 @testset "PandaModels.jl" begin
 
         include("input.jl")
         include("call_powermodels.jl")
-        include("vd.jl")
-        
+        include("call_pandamodels.jl")
+
 end

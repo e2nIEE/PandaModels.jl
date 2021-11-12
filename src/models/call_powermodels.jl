8f339bcb24495558461f087ff0e1bd843d78d7ef
Maryam Majidi <maryam.majidi@uni-kassel.de>
2021-11-12 18:12:42 +0100

    update:silence, solver, pf

--- src/models/call_powermodels.jl
+++ src/models/call_powermodels.jl
@@ -1,17 +1,39 @@
-function run_powermodels_pf(json_path)
-    _PM.silence()
-    pm = load_pm_from_json(json_path)
-    model = get_model(pm["pm_model"])
+# function run_powermodels_pf_nativ(json_path)
+#
+#     active_powermodels_silence!(pm)
+#
+#     pm = load_pm_from_json(json_path)
+#     model = pm["pm_model"]
+#
+#     # add result to net data
+#     _PM.update_data!(pm, result["solution"])
+#
+#     # calculate branch power flows
+#     if pm["pm_model"] == "ACNativ"
+#         _PM.compute_ac_pf(pm)
+#         # add result to net data
+#         _PM.update_data!(pm, result["solution"])
+#         flows = _PM.calc_branch_flow_ac(pm)
+#     elseif pm["pm_model"] == "DCNativ":
+#         _PM.compute_dc_pf(pm)
+#         # add result to net data
+#         _PM.update_data!(pm, result["solution"])
+#         flows = _PM.calc_branch_flow_dc(pm)
+#     end
+#
+#     # add flow to net and result
+#     _PM.update_data!(result["solution"], flows)
+#     _PM.update_data!(pm, flows)
+#
+#     return result
+# end
 
-    solver = get_solver(
-        pm["pm_solver"],
-        pm["pm_nl_solver"],
-        pm["pm_mip_solver"],
-        pm["pm_log_level"],
-        pm["pm_time_limit"],
-        pm["pm_nl_time_limit"],
-        pm["pm_mip_time_limit"],
-    )
+function run_powermodels_pf(json_path)
+    pm = load_pm_from_json(json_path)
+    active_powermodels_silence!(pm)
+    pm = check_powermodels_data!(pm)
+    model = get_model(pm["pm_model"])
+    solver = get_solver(pm)
 
     result = _PM.run_pf(
         pm,
@@ -19,106 +41,102 @@ function run_powermodels_pf(json_path)
         solver,
         setting = Dict("output" => Dict("branch_flows" => true)),
     )
+
+    # add result to net data
+    _PM.update_data!(pm, result["solution"])
+    # calculate branch power flows
+    if string(model) == "PowerModels.ACPPowerModel"
+        flows = _PM.calc_branch_flow_ac(pm)
+    else
+        flows = _PM.calc_branch_flow_dc(pm)
+    end
+    # add flow to net and result
+    _PM.update_data!(result["solution"], flows)
+    _PM.update_data!(pm, flows)
+
     return result
 end
 
 function run_powermodels_opf(json_path)
-    # _PM.silence()
     pm = load_pm_from_json(json_path)
+    active_powermodels_silence!(pm)
+    pm = check_powermodels_data!(pm)
     model = get_model(pm["pm_model"])
+    solver = get_solver(pm)
 
-    solver = get_solver(
-        pm["pm_solver"],
-        pm["pm_nl_solver"],
-        pm["pm_mip_solver"],
-        pm["pm_log_level"],
-        pm["pm_time_limit"],
-        pm["pm_nl_time_limit"],
-        pm["pm_mip_time_limit"],
-        pm["pm_tol"],
-    )
-
-
-    if haskey(pm["branch"]["1"], "c_rating_a")
-        for (key, value) in pm["gen"]
-            # value["pmin"] = 0
-            value["pmax"] *= 0.01
-            value["qmax"] *= 0.01
-            value["qmin"] *= 0.01
-            value["pg"] *= 0.01
-            value["qg"] *= 0.01
-            value["cost"] *= 100
-        end
-
-        for (key, value) in pm["branch"]
-            value["c_rating_a"] *= 0.01
-        end
-
-        for (key, value) in pm["load"]
-            value["pd"] *= 0.01
-            value["qd"] *= 0.01
-        end
-
-        result = _PM._run_opf_cl(
-            pm,
-            model,
-            solver,
-            setting = Dict("output" => Dict("branch_flows" => true)),
-        )
-    else
         result = _PM.run_opf(
             pm,
             model,
             solver,
             setting = Dict("output" => Dict("branch_flows" => true)),
         )
-    end
 
-    # result["termination_status"] = string(result["termination_status"])
-    # result["dual_status"] = string(result["dual_status"])
-    # result["primal_status"] = string(result["primal_status"])
     return result
 end
 
-function run_powermodels_custom(json_path)
-    _PM.silence()
-    pm = load_pm_from_json(json_path)
-    model = get_model(pm["pm_model"])
+# function run_powermodels_opf_current_limit(json_path)
+#
+#     active_powermodels_silence!(pm)
+#
+#     pm = load_pm_from_json(json_path)
+#     model = get_model(pm["pm_model"])
+#
+#     solver = get_solver(pm)
+#
+#     if haskey(pm["branch"]["1"], "c_rating_a")
+#         for (key, value) in pm["gen"]
+#             # value["pmin"] = 0
+#             value["pmax"] *= 0.01
+#             value["qmax"] *= 0.01
+#             value["qmin"] *= 0.01
+#             value["pg"] *= 0.01
+#             value["qg"] *= 0.01
+#             value["cost"] *= 100
+#         end
+#
+#         for (key, value) in pm["branch"]
+#             value["c_rating_a"] *= 0.01
+#         end
+#
+#         for (key, value) in pm["load"]
+#             value["pd"] *= 0.01
+#             value["qd"] *= 0.01
+#         end
+#
+#         result = _PM._run_opf_cl(
+#             pm,
+#             model,
+#             solver,
+#             setting = Dict("output" => Dict("branch_flows" => true)),
+#         )
+#
+#     return result
+# end
 
-    solver = get_solver(
-        pm["pm_solver"],
-        pm["pm_nl_solver"],
-        pm["pm_mip_solver"],
-        pm["pm_log_level"],
-        pm["pm_time_limit"],
-        pm["pm_nl_time_limit"],
-        pm["pm_mip_time_limit"],
-        pm["pm_tol"],
-    )
-
-    result = _PM.run_pf(pm, model, solver)
-    # add branch flows
-    _PM.update_data!(pm, result["solution"])
-    flows = _PM.calc_branch_flow_ac(pm)
-    _PM.update_data!(result["solution"], flows)
-    return result
-end
+# function run_powermodels_custom(json_path)
+#
+#     pm = load_pm_from_json(json_path)
+#
+#     active_powermodels_silence!(pm)
+#     pm = check_powermodels_data!(pm)
+#
+#     model = get_model(pm["pm_model"])
+#     solver = get_solver(pm)
+#
+#     result = _PM.run_pf(pm, model, solver)
+#     # add branch flows
+#     _PM.update_data!(pm, result["solution"])
+#     flows = _PM.calc_branch_flow_ac(pm)
+#     _PM.update_data!(result["solution"], flows)
+#     return result
+# end
 
 function run_powermodels_tnep(json_path)
-    _PM.silence()
     pm = load_pm_from_json(json_path)
+    active_powermodels_silence!(pm)
+    pm = check_powermodels_data!(pm)
     model = get_model(pm["pm_model"])
-
-    solver = get_solver(
-        pm["pm_solver"],
-        pm["pm_nl_solver"],
-        pm["pm_mip_solver"],
-        pm["pm_log_level"],
-        pm["pm_time_limit"],
-        pm["pm_nl_time_limit"],
-        pm["pm_mip_time_limit"],
-        pm["pm_tol"],
-    )
+    solver = get_solver(pm)
 
     result = _PM.run_tnep(
         pm,
@@ -130,52 +148,17 @@ function run_powermodels_tnep(json_path)
 end
 
 function run_powermodels_ots(json_path)
-    _PM.silence()
     pm = load_pm_from_json(json_path)
+    active_powermodels_silence!(pm)
+    pm = check_powermodels_data!(pm)
     model = get_model(pm["pm_model"])
-
-    solver = get_solver(
-        pm["pm_solver"],
-        pm["pm_nl_solver"],
-        pm["pm_mip_solver"],
-        pm["pm_log_level"],
-        pm["pm_time_limit"],
-        pm["pm_nl_time_limit"],
-        pm["pm_mip_time_limit"],
-        pm["pm_tol"],
-    )
+    solver = get_solver(pm)
 
     result = _PM.run_ots(
         pm,
         model,
         solver,
         setting = Dict("output" => Dict("branch_flows" => true)),
-    )
-    return result
-end
-
-function run_pandamodels_vd(json_path)
-    _PM.silence()
-    pm = load_pm_from_json(json_path)
-    model = get_model(pm["pm_model"])
-
-    solver = get_solver(
-        pm["pm_solver"],
-        pm["pm_nl_solver"],
-        pm["pm_mip_solver"],
-        pm["pm_log_level"],
-        pm["pm_time_limit"],
-        pm["pm_nl_time_limit"],
-        pm["pm_mip_time_limit"],
-        pm["pm_tol"],
-    )
-
-    result = _run_vd(
-        pm,
-        model,
-        solver,
-        setting = Dict("output" => Dict("branch_flows" => true)),
-        ext = extract_params!(pm),
     )
     return result
 end

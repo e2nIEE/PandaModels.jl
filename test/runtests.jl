f03f83e53413c6d096128293c03d1283dbe0a5fe
Maryam Majidi <maryam.majidi@uni-kassel.de>
2021-12-21 16:25:45 +0100

    update

--- test/runtests.jl
+++ test/runtests.jl
@@ -8,12 +8,20 @@ pdm_path = joinpath(dirname(pathof(PandaModels)), "..")
 data_path = joinpath(pdm_path, "test", "data")
 
 case_pm = joinpath(data_path, "test_pm.json")
+
 case_pf_ac = joinpath(data_path, "test_pf_ac.json")
+case_pf_ac_native = joinpath(data_path, "test_pf_ac_native.json")
+case_pf_dc_native = joinpath(data_path, "test_pf_dc_native.json")
+
 case_opf_ac = joinpath(data_path, "test_opf_ac.json")
 case_opf_cl = joinpath(data_path, "test_opf_cl.json")
+
 case_tnep_ac = joinpath(data_path, "test_tnep_ac.json")
+
 case_ots_dc = joinpath(data_path, "test_ots_dc.json")
+
 case_vd = joinpath(data_path, "test_vd.json")
+
 
 @testset "PandaModels.jl" begin
 

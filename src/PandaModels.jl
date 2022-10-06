9feb4e9c037daad81182c85ee71886d33c131f0d
Zheng Liu <zheng.liu@uni-kassel.de>
2022-10-06 13:41:39 +0200

    removed code for debug

--- src/PandaModels.jl
+++ src/PandaModels.jl
@@ -32,9 +32,7 @@ export run_powermodels_pf,
     run_pandamodels_ploss,
     run_pandamodels_vstab_test,
     run_pandamodels_qflex_test,
-    run_pandamodels_ploss,
-    run_pandamodels_ploss_test,
-    run_powermodels_storage
+    run_pandamodels_ploss_test
 
 include("input/pp_to_pm.jl")
 include("input/tools.jl")
@@ -45,6 +43,4 @@ include("models/call_pandamodels.jl")
 include("models/call_powermodels.jl")
 include("models/run_pm_vstab_dev.jl")
 include("models/run_pm_qflex_dev.jl")
-include("models/run_pm_ploss_dev.jl")
-include("models/run_pm_storage_dev.jl")
 end
--- src/models/vstab.jl
+++ src/models/vstab.jl
@@ -1,4 +1,4 @@
-export _run_vstab, _run_multi_vstab, _run_vstab_limq
+export _run_vstab, _run_multi_vstab
 
 """
 run optimization for maintaining voltage setpoints
@@ -116,66 +116,4 @@ function objective_multi_vstab(pm::_PM.AbstractPowerModel)
         sum((var(pm, nw, :vm, content["element_index"]) - content["value"])^2 for (i, content) in pm.ext[:setpoint_v])
         for nw in timestep_ids)
             )
-end
-
-
-function _run_vstab_limq(file, model_type::_PM.Type, optimizer; kwargs...)
-    return _PM.solve_model(file, model_type, optimizer, _build_vstab_limq; kwargs...)
-end
-
-"""
-given a JuMP model and a PowerModels network data structure,
-builds an "maintainig voltage setpoints" formulation with limited reactive power
-based on the given data and returns the JuMP model
-"""
-
-function _build_vstab_limq(pm::_PM.AbstractPowerModel)
-
-    _PM.variable_bus_voltage(pm)
-    _PM.variable_gen_power(pm)
-    _PM.variable_branch_power(pm)
-    _PM.variable_dcline_power(pm, bounded = false) # TODO: why false?
-
-
-    objective_vstab_limq(pm)
-
-    _PM.constraint_model_voltage(pm)
-
-    for i in _PM.ids(pm, :ref_buses)
-        _PM.constraint_theta_ref(pm, i)
-    end
-
-    for i in _PM.ids(pm, :bus)
-        _PM.constraint_power_balance(pm, i)
-    end
-
-    for (i, branch) in _PM.ref(pm, :branch)
-        _PM.constraint_ohms_yt_from(pm, i)
-        _PM.constraint_ohms_yt_to(pm, i)
-
-        _PM.constraint_thermal_limit_from(pm, i)
-        _PM.constraint_thermal_limit_to(pm, i)
-    end
-
-    for i in _PM.ids(pm, :dcline)
-        _PM.constraint_dcline_power_losses(pm, i)
-    end
-end
-
-function objective_vstab_limq(pm::_PM.AbstractPowerModel)
-
-    if haskey(pm.ext, :obj_factors)
-        if length(pm.ext[:obj_factors]) == 2
-            fac1 = pm.ext[:obj_factors]["fac_1"]
-            fac2 = pm.ext[:obj_factors]["fac_2"]
-        end
-    else
-        fac1 = 0.8
-        fac2 = 1-fac1
-    end
-
-    return JuMP.@objective(pm.model, Min,
-       fac1 * sum((var(pm, :vm, content["element_index"]) - content["value"])^2 for (i, content) in pm.ext[:setpoint_v])
-       +
-       fac2 * sum((var(pm, :qg, content)-0)^2 for (i, content) in pm.ext[:gen_and_controllable_sgen]))
 end
